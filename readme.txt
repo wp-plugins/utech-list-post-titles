eca5c490df70770e8aacfa4ec0abba2e42f211e9
Meini <Meini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-26 03:33:27 +0000

    git-svn-id: https://plugins.svn.wordpress.org/utech-list-post-titles/trunk@267610 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== Plugin Name ===
+Contributors: Meini - Utech Computer Solutions, Sachethan G Reddy
+Tags: latest posts, list latest posts, list posts titles, list latest post titles, latest news, list news, posts, news, list blogs, bogs, blog entries, page 
+Requires at least: 2.6
+Tested up to: 3.0
+Stable tag: 2.0
+
+List your latest posts by category using simple shortcode.
+
+== Description ==
+
+Add a list of your latest blog entries on any page. 
+With simple shortcode you can place a list of your latest news (or other blog category) on any page or post.
+This is a simple plugin base on a query by Sachethan G Reddy that fetches the latest posts titles from any given category.
+
+== Installation ==
+
+The plugin is simple to install:
+
+1. You can use the WordPress installer, or you can install the plugin
+manually by uploading the entire plugin folder `utech_list_post_titles` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place shortcode on your page, eg. `[utech_latest_posts category='news']`
+
+== Frequently Asked Questions ==
+
+= The shortcode does not return anything =
+
+This is most likely because the category name you used in your shortcode is not present or misspelled.
+
+= Any other question = 
+
+Please contact the author at http://www.utechworld.com
+
+
+== Documentation ==
+
+Use the following shortcode anywhere on your page or post following the examples below:
+`[utech_latest_posts category='news']`
+replace the word news in this example with the category that you would like to use.
+
+`[utech_latest_posts category='news' limit=10]`
+this example limits the number of results to 10 instead of the default which is 5 entries
+
+
+== Settings ==
+none
+
+== Changelog ==
+2.0 [July 11, 2010]
+- newly released by Meini, Utech Computer Solutions
+- introduced the use of shortcodes
+
+1.0 [July 07, 2009]
+- first release by Sachethan G Reddy
---
+++ utech_list_post_titles.php
@@ -0,0 +1,60 @@
+<?php
+/*
+Plugin Name: Utech List Posts Titles
+Plugin URI: http://www.utechworld.com/projects/list-posts-for-wp/
+Description: List your latest posts by category using simple shortcode. E.g [utech_latest_posts category='news']
+Version: 2.0
+Author: Meini based on work by Sachethan G Reddy
+Author URI: http://www.utechworld.com
+License: GPL2
+*/
+
+/*  Copyright 2010  Meini  (http://www.utechworld.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307USA
+*/
+
+//[latest_posts]
+add_shortcode('utech_latest_posts', 'utech_sc_get_latest_posts');
+
+function utech_sc_get_latest_posts($atts){
+	extract(shortcode_atts(array(
+		'category' => '',
+		'limit' => 5,
+		), $atts));
+		return utech_get_latest_posts($category, $limit);
+}
+
+function utech_get_latest_posts($category, $limit=5){
+	$start_tag='<li>'; $end_tag = '</li>';
+	global $wpdb;
+	$category = str_replace(",", "','", $category);
+	$category = "'" . $category . "'";
+	$query = "select wtr.object_id , wp.id, wp.post_title, wt.name, wp.post_modified, wp.post_date, wp.post_name from " . $wpdb->prefix."posts wp, " . $wpdb->prefix."term_relationships wtr, " . $wpdb->prefix."term_taxonomy wtt, " . $wpdb->prefix."terms wt where wp.id = wtr.object_id and wtr.term_taxonomy_id = wtt.term_taxonomy_id and wtt.term_id = wt.term_id and upper(wt.name) in (" . strtoupper($category) . ") AND wtt.taxonomy like 'category' AND wp.post_status = 'publish' AND wp.post_type like 'post' AND wp.post_password ='' ORDER BY wp.post_date DESC LIMIT ". $limit . ";" ;
+	$latest_posts = $wpdb->get_results($query);
+	$result = '<div class="utech-latest-posts"><ul>';
+	if ($latest_posts) {
+		foreach ($latest_posts as $latest_post) {
+			$post_title = $latest_post->post_title;
+			$permaLink = get_permalink($latest_post->id);
+			$html = $start_tag . "<a href=\"" .  $permaLink . "\" title=\"" . $post_title . "\" rel='bookmark'>" .$post_title  . " </a>" . $end_tag;
+			$result .= $html; 
+		}
+	}
+	$result .= "</ul></div>";
+	return $result;
+	
+}
+?>
